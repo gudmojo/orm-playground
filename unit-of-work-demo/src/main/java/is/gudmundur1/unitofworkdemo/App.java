d083facbb9e7fe869b60eea5f09242838f75ec22
gudmojo <gudmundur@handpoint.com>
2016-07-21 16:06:25 +0000

    Fix formatting

--- unit-of-work-demo/src/main/java/is/gudmundur1/unitofworkdemo/App.java
+++ unit-of-work-demo/src/main/java/is/gudmundur1/unitofworkdemo/App.java
@@ -4,10 +4,10 @@ package is.gudmundur1.unitofworkdemo;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
     }
 }
